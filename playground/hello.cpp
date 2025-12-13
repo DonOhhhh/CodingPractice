4ffd33c54d1b8e096c4a1f5f3771e92038acb856
dongjoon.oh <dongjoon.oh@hear.com>
2025-12-13 18:34:15 +0900

    feat: add basic array manipulation functions (insert, delete, append, prepend, pop) and their tests in C++ and Rust.

--- playground/hello.cpp
+++ playground/hello.cpp
@@ -1,21 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int func2(const vector<int> &v1, int n) {
-  vector<int> freq(100, 0);
-  for (auto i : v1) {
-    if (freq[100 - i])
-      return 1;
-    freq[i]++;
-  }
-  return 0;
+void my_insert(int *arr, int &len, int pos, int val) {
+  for (int i = len; i > pos; i--)
+    arr[i] = arr[i - 1];
+  arr[pos] = val;
+  len++;
 }
 
-int main(void) {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cout << func2({1, 23, 53, 77, 60}, 5) << '\n';
-  cout << func2({1, 52, 48}, 3) << '\n';
-  cout << func2({50, 42}, 2) << '\n';
-  cout << func2({4, 13, 63, 87}, 4) << '\n';
+void my_delete(int *arr, int &len, int pos) {
+  len--;
+  for (int i = pos; i < len; i++)
+    arr[i] = arr[i + 1];
+}
+
+void my_append(int *arr, int &len, int val) { arr[len++] = val; }
+
+void my_prepend(int *arr, int &len, int val) { my_insert(arr, len, 0, val); }
+
+int my_pop_front(int *arr, int &len) {
+  int ret = arr[0];
+  my_delete(arr, len, 0);
+  return ret;
+}
+
+int my_pop_back(int *arr, int &len) {
+  int ret = arr[len - 1];
+  len--;
+  return ret;
+}
+
+void print_array(int *arr, int len) {
+  for (int i = 0; i < len; i++)
+    cout << arr[i] << ' ';
+  cout << "\n";
+}
+
+int main() {
+  int arr[100] = {10, 20, 30};
+  int len = 3;
+  my_insert(arr, len, 1, 40); // 10 40 20 30
+  print_array(arr, len);
+
+  my_delete(arr, len, 2); // 10 40 30
+  print_array(arr, len);
+
+  my_append(arr, len, 50); // 10 40 30 50
+  print_array(arr, len);
+
+  my_prepend(arr, len, 60); // 60 10 40 30 50
+  print_array(arr, len);
+
+  cout << my_pop_front(arr, len) << endl; // 60
+  print_array(arr, len);                  // 10 40 30 50
+
+  cout << my_pop_back(arr, len) << endl; // 50
+  print_array(arr, len);                 // 10 40 30
 }
