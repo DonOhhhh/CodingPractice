fcca974db305fb777a8a29b36d86df7b441f07d0
dongjoon.oh <dongjoon.oh@hear.com>
2026-01-18 23:27:32 +0900

    refactor: simplify code by adding `using namespace std;` and removing `std::` prefixes

--- problems/Codeforces/339A/solution1.cpp
+++ problems/Codeforces/339A/solution1.cpp
@@ -3,26 +3,28 @@
 #include <string>
 #include <vector>
 
-int main() {
-    std::string s;
-    std::cin >> s;
+using namespace std;
 
-    std::string nums;
+int main() {
+    string s;
+    cin >> s;
+
+    string nums;
     // Extract only digits
     for (char c : s) {
         if (c != '+') nums.push_back(c);
     }
 
     // Sort the digits
-    std::ranges::sort(nums);
+    ranges::sort(nums);
 
     // Print with '+'
-    std::vector<char> v;
+    vector<char> v;
     for (size_t i = 0; i < nums.size(); ++i) {
         v.push_back(nums[i]);
         v.push_back('+');
     }
 
-    std::cout.write(v.data(), v.size() - 1);
+    cout.write(v.data(), v.size() - 1);
     return 0;
 }
